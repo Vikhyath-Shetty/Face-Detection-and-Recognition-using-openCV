abc64747c5d33a71a7897f52d8bc57953fb14c02
Vikhyath V Shetty <vikhyath.v.shetty@gmail.com>
2025-09-16 23:58:49 +0530

    In progress

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import argparse
+import logging
 from src.utils import cameraType
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
 
 def main() -> None:
     parser = argparse.ArgumentParser("Face detection and recognition")
     parser.add_argument("mode", choices=["capture", "train", "detect"],
                         help="Select operation: 'Capture' to collect face images, 'train' to build the recoginition modelm, 'detect' to run real time face recognition")
     parser.add_argument("-c", "--camera", default=0, type=cameraType,
-                        help="Camera source: pass an integer(0,1,2) for local webcam, or an HTTP/RTSP URL for IP camera/stream")
+                        help="Camera source: pass an integer(0,1,2) for local webcam, or an HTTP/RTSP URL for IP camera/stream,(default=0)")
+    parser.add_argument("-d", "--detector", default="haar", type=str,
+                        help="Face Detector: 'haar' to use Haar detector, 'hog' to use HOG+SVM detector,(default=haar)")
     args = parser.parse_args()
-    print(f"Mode is: {args.mode}, Camera is : {args.camera}, type: {type(args.camera)}")
 
 
 if __name__ == '__main__':
--- src/capture_faces.py
+++ src/capture_faces.py
@@ -1,4 +1,16 @@
 import cv2 as cv
+import logging
 
-def capture():
-    cv.VideoCapture(0)
+def capture(camera: str | int) -> None:
+    cap = cv.VideoCapture(camera)
+    if not cap.isOpened():
+        raise RuntimeError(f"Failed to open camera source: {camera}")
+    while True:
+        ret,frame = cap.read()
+        if not ret:
+            logging.warning("Failed to grab frame...")
+            continue
+        
+
+
+        
